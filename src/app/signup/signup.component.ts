1e316d936c27852282e2fa302f6e1f2be3e039df
Maryann-01 <amaraizummesoma@gmail.com>
2025-06-12 17:07:26 +0100

    feat(add):password validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -19,12 +19,70 @@ export class SignupComponent {
   confirmPassword: string = '';
   termsAccepted: boolean = false;
   showSuccessModal: boolean = false;
+  passwordError: string = '';
+  confirmPasswordError: string = '';
 
   constructor(private http: HttpClient, private router: Router) {}
 
+  validatePassword(password: string): boolean {
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const minLength = password.length >= 8;
+
+    if (!minLength) {
+      this.passwordError = 'Password must be at least 8 characters long';
+      return false;
+    }
+    if (!hasUpperCase) {
+      this.passwordError = 'Password must contain at least one uppercase letter';
+      return false;
+    }
+    if (!hasLowerCase) {
+      this.passwordError = 'Password must contain at least one lowercase letter';
+      return false;
+    }
+    if (!hasNumbers) {
+      this.passwordError = 'Password must contain at least one number';
+      return false;
+    }
+    if (!hasSpecialChar) {
+      this.passwordError = 'Password must contain at least one special character';
+      return false;
+    }
+
+    this.passwordError = '';
+    return true;
+  }
+
+  validateForm(): boolean {
+    let isValid = true;
+    this.passwordError = '';
+    this.confirmPasswordError = '';
+    if (!this.password) {
+      this.passwordError = 'Password is required';
+      isValid = false;
+    } else if (!this.validatePassword(this.password)) {
+      isValid = false;
+    }
+    if (!this.confirmPassword) {
+      this.confirmPasswordError = 'Please confirm your password';
+      isValid = false;
+    } else if (this.password !== this.confirmPassword) {
+      this.confirmPasswordError = 'Passwords do not match';
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
   onSubmit(event: Event): void {
     event.preventDefault();
-    
+    if (!this.validateForm()) {
+      return;
+    }
+
     const firstName = this.name.split(' ')[0]; 
 
     if (this.password === this.confirmPassword && this.termsAccepted) {
